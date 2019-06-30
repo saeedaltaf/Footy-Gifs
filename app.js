17bda268a9417c7a96d6ed825ec3961399dc1a7f
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-29 23:25:09 -0400

    updated pseudocode in app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+//Pseudo Code
+//Create an array of footy gif topics
+//Create a button for each gif topic
+//Use a for loop to append a button to each string in the array
+//Grab 10 static, non-animated GIFS from GIPHY API
+//They should animate on click (play/pause)
+//Display rating of each GIF under the GIF
+//Make AJAX call that takes each topic in array and remakes the buttons on the page
