016379fdd7b48f4a423bc2688567957bd40031ce
Saeed Altaf <saeedaltaf@gmail.com>
2019-06-30 20:52:06 -0400

    Updated QueryURL to make it work, updated response to make it work (previously undefined)

--- app.js
+++ app.js
@@ -8,54 +8,76 @@
 //Make AJAX call that takes each topic in array and remakes the buttons on the page
 $(document).ready(function () {
 
-var footyArray = ["Arsenal", "Chelsea", "Barcelona", "Manchester United", "AC Milan",
-    "Mario Balotelli", "Luis Suarez", "Joey Barton", "Vinnie Jones"];
+    var footyArray = ["Arsenal", "Chelsea", "Barcelona", "Manchester United", "AC Milan",
+        "Mario Balotelli", "Luis Suarez", "Joey Barton", "Vinnie Jones"];
 
-function createButtons() {
-    //To delete buttons before adding new buttons (avoids repeat buttons):
-    $("#buttons-view").empty();
+    function createButtons() {
+        //To delete buttons before adding new buttons (avoids repeat buttons):
+        $("#buttons-view").empty();
 
-    //Loop through the array to generate buttons for each string:
-    for (var i = 0; i < footyArray.length; i++) {
-        var createButtons = $("<button>");
-        createButtons.addClass("footy");
-        createButtons.attr("data-name", footyArray[i]);
-        createButtons.text(footyArray[i]);
+        //Loop through the array to generate buttons for each string:
+        for (var i = 0; i < footyArray.length; i++) {
+            var createButtons = $("<button>");
+            createButtons.addClass("footy");
+            createButtons.attr("data-name", footyArray[i]);
+            createButtons.text(footyArray[i]);
 
-        $("#buttons-view").append(createButtons);
-        $("#footy-input").val("");
+            $("#buttons-view").append(createButtons);
+            $("#footy-input").val("");
+        }
     }
-}
 
-//Creates button for user input
-$("#add-footy").on("click", function (event) {
-    //So form won't submit itself, user can click submit or press enter
-    event.preventDefault();
-    //push the input to the array:
-    var footyUser = $("#footy-input").val().trim();
-    footyArray.push(footyUser);
+    //Creates button for user input
+    $("#add-footy").on("click", function (event) {
+        //So form won't submit itself, user can click submit or press enter
+        event.preventDefault();
+        //push the input to the array:
+        var footyUser = $("#footy-input").val().trim();
+        footyArray.push(footyUser);
+
+        createButtons();
+    })
 
     createButtons();
-})
 
-createButtons();
+    //AJAX Call to pull GIPHY gifs based on specific button click:
+    $(document).on("click", ".footy", function (event) {
+        var response = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + response + "&api_key=eM3vuQQaXtYtAStZNGXUC7wA1T5Z46FB&limit=10";
 
-//AJAX Call to pull GIPHY gifs based on specific button click:
-$(document).on("click", ".footy", function (event) {
-    var searchInput = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchInput + "&api_key=eM3vuQQaXtYtAStZNGXUC7wA1T5Z46FB&limit=10";
+        // console.log(queryURL);
+        console.log(queryURL);
+        console.log(response);
 
-    // console.log(queryURL);
-    // console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var newFootyDiv = $("<div>");
-        newFootyDiv.append("Rating: " + response.slug);
-        newFootyDiv.append("<img src='" + response.images + "' />");
-        $("#gifHolder").append(newFootyDiv);
+            //storing the array as a variable name:
+            var giphy = response.data;
+            $("#gifHolder").empty();
+
+            //Loop through the array in queryURL:
+            for (var i = 0; i < giphy.length, i++;) {
+                var footyGifs = $("#gifHolder");
+
+                //GIF Rating:
+                var rate = $("<p>").text("Rating: " + giphy[i].rating + "</p>");
+                var footyGifImage = $("<img giphy-state ='animate'>");
+
+                footyGifImage.attr('src', giphy[i].fixed_height_still.url);
+                footyGifImage.attr('data-animate', giphy[i].images.fixed_height.url)
+                footyGifImage.attr('data-still', giphy[i].images.fixed_height_still.url)
+                footyGifImage.attr('class', 'gif img-responsive')
+
+
+                footyGifs.append(rate);
+                footyGifs.append(footyGifImage);
+
+            }
+
+        })
     })
-})
 });
